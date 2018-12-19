26239ba8e732dc63347cf132ededeadec379ebae
adadesions <adadesions@gmail.com>
2018-12-20 02:40:28 +0700

    Add priority diaplsy and deadline display styling

--- adanotes/note.py
+++ adanotes/note.py
@@ -2,6 +2,8 @@ import csv
 import uuid
 import click
 
+from datetime import datetime
+
 keys = ['id', 'content', 'end_date', 'end_time', 'priority', 'created_datetime']
 
 
@@ -30,6 +32,41 @@ def adding(data):
     
     return False
 
+
+def priority_display(priority_text):
+    text = str.lower(priority_text)
+    colors = {
+        'high': 'red',
+        'medium': 'white',
+        'low': 'black'
+    }
+    click.secho(f'Priority: {priority_text}', fg=colors.get(text, 'black'), bold=True)
+
+
+def deadline_display(end_date, end_time):
+    now = datetime.now().strftime('%Y-%m-%d-%H-%M')
+    cur_datetime = now.split('-')
+
+    end_datetime = end_date.split('-')[::-1]+end_time.split(':')
+    for i, end in enumerate(end_datetime):
+        end = int(end)
+        cur = int(cur_datetime[i])
+        if cur <= end:
+            # End year greater than current year
+            if i == 0 and cur < end:
+                break
+
+            # End hours greater than current hours, no need to check minutes
+            if i == 3 and cur < end:
+                break
+        else:
+            click.secho('Deadline:', fg='white', nl=False )
+            click.secho(f'{end_date} {end_time}', fg='white', bg='red')
+            return False
+
+    click.secho('Deadline:', fg='white', nl=False )
+    click.secho(f'{end_date} {end_time}', fg='white', bg='green')
+
 def display():
     # TODO: Display sorted by deadline
     click.secho('AdaNotes showing your meaningful', fg='green', bold=True)
@@ -37,13 +74,13 @@ def display():
     with open('data/store.csv', newline='') as file:
         note_reader = csv.DictReader(file)
         for row in note_reader:
+            click.secho(f'ID: {row["id"]}', fg='yellow', bg='blue', bold=True)
+            priority_display(row['priority'])
             click.secho(f'Note: {row["content"]}', fg='yellow')
             click.secho(f'Start: {row["created_datetime"]}', fg='magenta')
-            click.secho('Deadline:', fg='green', nl=False )
-            click.secho(f'{row["end_date"]}', fg='green', bg='black', nl=False)
-            click.secho(f'  {row["end_time"]}', fg='green', bg='black')
             
-            # {row["end_time"]}')
+            deadline_display(row['end_date'], row['end_time'])
+            
             print(
                 f'{"-"*92}\n'
             )
